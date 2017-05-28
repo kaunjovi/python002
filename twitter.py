1dd9e21f54f7c88ba9a35d963365fc0681056dc1
kaunjovi <kaunjovi@gmail.com>
2017-05-28 22:14:20 +0530

    read the twitter timeline. put the twitter api keys in properties file.

--- twitter.py
+++ twitter.py
@@ -1,22 +1,36 @@
 import tweepy
- 
-# Consumer keys and access tokens, used for OAuth
-consumer_key = 'km9ZVmO42qCtcjcukbk4B54qS'
-consumer_secret = 'NeURzc08wrGhA0emAxpjdJx8MOK4RsyXBDl4VDADQ6rLKTQub6'
-access_token = '719722333632643076-4fNGpXADGQgapb1OAk3bMAQRXfNZbqg'
-access_token_secret = 'qRwx3YMeIB42iDpInontLtVuMaMnWJTmlr2twOsArbcnc'
+import configparser
 
- 
+
+# Consumer keys and access tokens, used for OAuth
+config = configparser.RawConfigParser()
+config.read('secret.properties')
+
+# print config.get('twitterapikeys', 'consumer_key')
+# print config.get('twitterapikeys', 'consumer_secret')
+# print config.get('twitterapikeys', 'access_token')
+# print config.get('twitterapikeys', 'access_token_secret')
+
+consumer_key = config.get('twitterapikeys', 'consumer_key')
+consumer_secret = config.get('twitterapikeys', 'consumer_secret')
+access_token = config.get('twitterapikeys', 'access_token')
+access_token_secret = config.get('twitterapikeys', 'access_token_secret')
+
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
- 
+
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
- 
-# Creates the user object. The me() method returns the user whose authentication keys were used.
+
+# Creates the user object. The me() method returns the user
 user = api.me()
- 
+
 print 'Name: ' + user.name
 print 'Location: ' + user.location
 print 'Friends: ' + str(user.friends_count)
+
+# Read the timeline
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    print status.text.encode('utf-8')
